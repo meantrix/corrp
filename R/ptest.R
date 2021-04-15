8fbb5060ba5ec1eca6a2ced99f420da27a85c652
igor-siciliani <igorschoeller@gmail.com>
2021-04-15 19:49:58 -0300

    ptest working version

--- R/ptest.R
+++ R/ptest.R
@@ -7,14 +7,22 @@
 #'@param num.s number of samples with replacement created with y numeric vector.
 #'@param alternative a character string specifying the alternative hypothesis,
 #'must be one of "two.sided" (default), "greater" or "less". You can specify just the initial letter.
-
+#' @example
+#' \dontrun{
+#' x = iris[[1]]; y = iris[[2]]
+#'
+#' ptest(x,y,FUN = mean ,alternative = 't')
+#'
+#' ptest(x,y,FUN = function(x,y) cor(x,y) ,alternative = 't')
+#'
+#'
+#'}
 #'@export
 #'
 ptest = function(x ,y,
                  FUN,
-                 sep = 1L,
                  alternative = c("two.sided", "less", "greater"),
-                 num.s = 20000, ... ) {
+                 num.s = 1000, ... ) {
 
 
   FUN = match.fun(FUN)
@@ -22,25 +30,24 @@ ptest = function(x ,y,
   fargs = formals(FUN)
   fargs = fargs[vapply(fargs, is.symbol, FUN.VALUE = TRUE)]
   fargs = names(fargs)
+  fargs = setdiff(fargs, "...")
   largs = length(fargs)
   if( largs > 2 ) stop( 'FUN maximum number of mandatory arguments is 2.' )
 
 
   alternative = substr(alternative,1,1)
   checkmate::assert_character(alternative,len = 1, pattern = "t|l|g")
-  checkmate::assert_logical(paired,len = 1)
-  checkmate::assert_logical(all.p,len = 1)
   checkmate::assert_number(num.s)
   stopifnot(is.numeric(x),is.numeric(y))
   stopifnot(length(x) == length(y))
 
 
-  obs = ifelse(largs > 2 , FUN(x,y), FUN(x) - FUN(y) )
+  obs = ifelse(largs > 1 , FUN(x,y), FUN(x) - FUN(y) )
   N = length(x)
   est = c()
   for(i in 1:num.s){
     y_i = sample(y, length(y), replace = T)
-    est = append(estimates, ifelse(largs > 2 , FUN(x,y), FUN(x) - FUN(y) ) )
+    est = append(est, ifelse(largs > 1 , FUN(x,y_i), FUN(x) - FUN(y_i) ) )
 
   }
 
@@ -60,11 +67,3 @@ ptest = function(x ,y,
 
 
 
-
-
-
-
-
-
-
-}
