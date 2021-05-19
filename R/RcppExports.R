def6b68f7b95a4da4ea7e2911fbf9a14f84c7b53
igor-siciliani <igorschoeller@gmail.com>
2021-05-19 18:42:42 -0300

    acca s3 class

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1,11 +1,75 @@
+#' @title Average correlation clustering algorithm
+#'
+#' @description A C++ implementation of the ACCA method
+#' that works directly with the correlation matrix derived from the \code{\link{corr_matrix}} function.
+#' In this sense, this implementation differs from the original,
+#' it works with mixed data and several correlation methods
+#'
+#' @param m  [\code{matrix(1)}]\cr correlation matrix from \code{\link{corr_matrix}}
+#' @param k [\code{integer(1)}]\cr number of clusters considered.
+#' @param maxrep [\code{integer(1)}]\cr maximum number of interactions without change in the clusters.
+#' @param maxiter [\code{integer(1)}]\cr maximum number of interactions.
+#'
+#' @author Igor D.S. Siciliani
+#'
+#' @keywords correlation , acca
+#'
+#' @references
+#' Bhattacharya, Anindya, and Rajat K. De.
+#' "Average correlation clustering algorithm (ACCA) for grouping of co-regulated
+#' genes with similar pattern of variation in their expression values."
+#' Journal of Biomedical Informatics 43.4 (2010): 560-568.
+#'
+#' @export
+#'
+acca = function(m,...) {
+UseMethod('acca',m)
+}
 
 
 
+acca.cmatrix <- function(m, k, maxrep = 2L, maxiter = 100L) {
 
-acca <- function(m, k, maxrep = 2L, maxiter = 100L) {
+  k = as.integer(k)
+  maxrep = as.integer(maxrep)
+  maxiter = as.integer(maxiter)
+  checkmate::assert_int(k)
+  checkmate::assert_int(maxrep)
+  checkmate::assert_int(maxiter)
+
     .Call(`_corrp_acca_main`, m, k, maxrep, maxiter)
 }
 
+#' @export
+#' @rdname acca
+acca.cmatrix <- function(m, k, maxrep = 2L, maxiter = 100L) {
+
+  k = as.integer(k)
+  maxrep = as.integer(maxrep)
+  maxiter = as.integer(maxiter)
+  checkmate::assert_int(k)
+  checkmate::assert_int(maxrep)
+  checkmate::assert_int(maxiter)
+
+  .Call(`_corrp_acca_main`, m, k, maxrep, maxiter)
+}
+
+#' @export
+#' @rdname acca
+acca.matrix <- function(m, k, maxrep = 2L, maxiter = 100L) {
+
+  warning("it is not an object of the 'cmatrix' class some results may go wrong.")
+
+
+  k = as.integer(k)
+  maxrep = as.integer(maxrep)
+  maxiter = as.integer(maxiter)
+  checkmate::assert_int(k)
+  checkmate::assert_int(maxrep)
+  checkmate::assert_int(maxiter)
+
+  .Call(`_corrp_acca_main`, m, k, maxrep, maxiter)
+}
 
 
 
