c8d8d9ee4181f9248c9cb2120989eb2ffdb90132
Igor-Siciliani <36808532+igor-siciliani@users.noreply.github.com>
2024-10-20 17:18:38 -0300

    feat - improve test coverage #12

--- tests/testthat/test-corr_rm.R
+++ tests/testthat/test-corr_rm.R
@@ -4,8 +4,14 @@ test_that("Tests on corr_rm", {
 
   corr <- corrp(df, comp = "g", alternative = "t")
   m <- corr_matrix(corr)
+
+  m2 <- m
+  class(m2) <- "matrix"
+
   df2 <- corr_rm(df = df, c = corr)
   df3 <- corr_rm(df = df, c = m)
-
+  expect_warning(df4 <- corr_rm(df = df, c = m2))
   expect_equal(df2, df3)
+  expect_equal(df2,df4)
+
 })
