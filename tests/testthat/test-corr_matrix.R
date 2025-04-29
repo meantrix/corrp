c5729f7212b0cec13f74176a533e34f386f70b07
Igor-Siciliani <36808532+igor-siciliani@users.noreply.github.com>
2025-04-29 08:51:08 -0300

    feat - improve coverage corr_matrix tests 1

--- tests/testthat/test-corr_matrix.R
+++ tests/testthat/test-corr_matrix.R
@@ -2,17 +2,30 @@ test_that("Tests on corr_matrix", {
   # data to tests
   df <- iris
 
-  corr <- corrp(df, comp = "g", alternative = "t")
-  m <- corr_matrix(corr, isig = F)
+  corr <- corrp(df, comp = "g", alternative = "t", parallel = F,  verbose = T)
+  corr2 <- corr
+  class(corr2) <- "list"
+
+  expect_error(corr_matrix(isig = T))
+
+  m1 <- corr_matrix(corr, isig = T)
+  m2 <- corr_matrix(corr, isig = F)
+  m3 <- corr_matrix(corr2, isig = T)
 
   # class test
-  expect_s3_class(m, "cmatrix")
+  expect_s3_class(m1, "cmatrix")
+  expect_s3_class(m2, "cmatrix")
+  expect_s3_class(m3, "cmatrix")
+
+  expect_false(all(m1 %in% m2))
+  expect_true(all(m1 %in% m3))
 
   si <- sample(seq_len(NROW(corr$data)), 3)
 
   inf <- corr$data[si, c("varx", "vary", "infer.value")]
 
   for (k in seq_len(NROW(inf))) {
-    expect_equal(m[as.character(inf$vary[k]), as.character(inf$varx[k])], inf$infer.value[k])
+    expect_equal(m1[as.character(inf$vary[k]), as.character(inf$varx[k])], inf$infer.value[k])
   }
+
 })
