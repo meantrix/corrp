9b53c8d99a4e61f40d109aaf051f7ae76af1a43b
Igor-Siciliani <36808532+igor-siciliani@users.noreply.github.com>
2025-04-29 11:02:11 -0300

    feat - improve coverage corr_matrix tests 2

--- tests/testthat/test-corr_matrix.R
+++ tests/testthat/test-corr_matrix.R
@@ -11,12 +11,15 @@ test_that("Tests on corr_matrix", {
   m1 <- corr_matrix(corr, isig = T)
   m2 <- corr_matrix(corr, isig = F)
   m3 <- corr_matrix(corr2, isig = T)
+  m4 <- corr_matrix(corr2, isig = T, col = "isig")
 
   # class test
   expect_s3_class(m1, "cmatrix")
   expect_s3_class(m2, "cmatrix")
   expect_s3_class(m3, "cmatrix")
+  expect_s3_class(m4, "cmatrix")
 
+  expect_true(all(m4 %in% c(T,F)))
   expect_false(all(m1 %in% m2))
   expect_true(all(m1 %in% m3))
 
