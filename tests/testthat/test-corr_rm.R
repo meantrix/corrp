a5a6490054bff504d5cfc50a3cd85c65c6fbe1f1
Igor-Siciliani <36808532+igor-siciliani@users.noreply.github.com>
2025-05-05 18:21:05 -0300

    fix - test corr_rm warning 1

--- tests/testthat/test-corr_rm.R
+++ tests/testthat/test-corr_rm.R
@@ -14,9 +14,8 @@ test_that("Tests on corr_rm", {
 
   df2 <- corr_rm(df = df, c = corr)
   df3 <- corr_rm(df = df, c = m)
-  df4 <- corr_rm(df = df, c = m)
-  df5 <- corr_rm(df = df, c = corr2)
   expect_warning(df4 <- corr_rm(df = df, c = m2))
+  expect_warning(df5 <- corr_rm(df = df, c = corr2))
 
   expect_equal(df2, df3)
   expect_equal(df2, df4)
